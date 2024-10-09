c9cc53178774d2d6774229afa9f963e356f6465b
jithin garapati <jithingarapati@gmail.com>
2024-10-09 14:23:04 -0500

    rate

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ['groq-sdk'],
+  experimental: {
+    serverComponentsExternalPackages: ['groq-sdk'], // Example of adding external packages
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '300mb', // This applies globally but isn't supported in next.config, so it must be set in individual API routes.
     },
-    webpack: (config) => {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-      })
-      return config
-    },
-  }
-  
+  },
+};
+
 export default nextConfig;
